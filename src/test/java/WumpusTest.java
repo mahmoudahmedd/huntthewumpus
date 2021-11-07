987c3301be7fc64bf24633d327b5e1b52b70e96b
Mahmoud Ahmed <mahmoud_ahmed@stud.fci-cu.edu.eg>
2021-11-07 16:12:33 +0200

    Change The arrow size from 3 to 5

---
+++ src/test/java/WumpusTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WumpusTest {
+
+    @Test
+    void validateNumberOfArrowsOnGameInit() {
+        //Given
+        int expectedArrowsNum = 5;
+
+        //when
+        Wumpus wumpus = new Wumpus();
+        wumpus.startNewGame();
+
+        //THEN
+        assertEquals(expectedArrowsNum, wumpus.numArrows);
+    }
+}
