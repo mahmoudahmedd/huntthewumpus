46f7e003e4f3af96c54896b150f9a70870969aad
Ahmed Misbah <ahmed.misbah@orange.com>
2021-11-11 12:11:56 +0200

    Formatting the code to make it more organized and readable

--- src/main/java/presenter/WumpusPresenter.java
+++ src/main/java/presenter/WumpusPresenter.java
@@ -5,23 +5,13 @@ import java.util.List;
 public interface WumpusPresenter {
     void startNewGame();
     void move();
-
     void setCurrRoom(int selectedRoom);
-
     int[][] getLinks();
-
     int getCurrRoom();
-
     boolean isGameOver();
-
     void shoot(int room);
-
-
     int getRoomSize();
-
     int getPlayerSize();
-
     int getNumArrows();
-
     List<String> getMessages();
 }
