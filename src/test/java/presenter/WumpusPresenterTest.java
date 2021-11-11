97ffcc1d610d53c45130cb8c05a1135dfcbdac47
Mahmoud Ahmed <mahmoud_ahmed@stud.fci-cu.edu.eg>
2021-11-11 12:26:36 +0200

    add testMoveToNonConnectedCave

--- src/test/java/presenter/WumpusPresenterTest.java
+++ src/test/java/presenter/WumpusPresenterTest.java
@@ -9,24 +9,35 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+TODO
+1- move to non connected cave.
+2- room has Wumpus.
+3- room has bat
+4- kill wumpus
+5- cave has pit
+
+ */
 @ExtendWith(MockitoExtension.class)
 class WumpusPresenterTest {
 
     @Mock
     RandomNumberGenerator randomNumberGenerator;
 
+
+    final int playerStartingCave = 0;
+    final int wumpusStartingCave = 18;
+    final int firstBatStartingCave = 19;
+    final int secondBatStartingCave = 13;
+    final int thirdBatStartingCave = 14;
+    final int firstPitCave = 3;
+    final int secondPitCave = 13;
+    final int numberOfCaves = 20;
+
     @Test
     public void testMovingPlayerToCave() {
-        final int playerStartingCave = 0;
-        final int wumpusStartingCave = 18;
-        final int firstBatStartingCave = 19;
-        final int secondBatStartingCave = 13;
-        final int thirdBatStartingCave = 14;
-        final int firstPitCave = 3;
-        final int secondPitCave = 13;
         final int playNextCave = 7;
-        final int numberOfCaves = 20;
-        final boolean gameIsNotOver=false;
+        final boolean gameIsNotOver = false;
 
         Mockito.when(randomNumberGenerator.generateNumber(numberOfCaves)).thenReturn(
                 playerStartingCave,
@@ -49,6 +60,33 @@ class WumpusPresenterTest {
         assertEquals(playNextCave,playCurrentRoom);
         assertEquals(isGameOver,gameIsNotOver);
 
+    }
+
+    @Test
+    public void testMoveToNonConnectedCave() {
+        final int playNextCave = 16;
+        final boolean gameIsNotOver = false;
+
+        Mockito.when(randomNumberGenerator.generateNumber(numberOfCaves)).thenReturn(
+                playerStartingCave,
+                wumpusStartingCave,
+                firstBatStartingCave,
+                secondBatStartingCave,
+                thirdBatStartingCave,
+                firstPitCave,
+                secondPitCave);
+
+        WumpusPresenter wumpusPresenter = new WumpusPresenterImpl(randomNumberGenerator);
+        wumpusPresenter.startNewGame();
+
+        wumpusPresenter.setCurrRoom(playNextCave);
+        wumpusPresenter.move();
+
+        final int playCurrentRoom = wumpusPresenter.getCurrRoom();
+        final boolean isGameOver = wumpusPresenter.isGameOver();
+
+        assertEquals(playerStartingCave, playCurrentRoom);
+        assertEquals(isGameOver, gameIsNotOver);
     }
 
 }
