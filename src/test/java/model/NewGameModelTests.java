f8baadd85e41c5216437f595816224931575ef2a
Ahmed Misbah <ahmed.misbah@orange.com>
2021-11-14 11:55:34 +0200

    Rename test to be more clear

--- src/test/java/model/NewGameModelTests.java
+++ src/test/java/model/NewGameModelTests.java
@@ -9,16 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class NewGameModelTests {
-
-
+    
     @Test
-    public void testGameMapInitialization() {
+    public void testGameMapInitializationProducedTheCorrectNumberOfCaves() {
         NewGame game = new NewGame();
         game.startGame();
         GameMap gameMap=game.getGameMap();
 
         assertEquals(gameMap.getCaves().size(),GameInitialConfigurations.NUMBER_OF_CAVES);
-
-        //TODO write an assertion to check if cave links are correct
     }
+
+    //TODO write a test to check that cave links are correct
+
 }
